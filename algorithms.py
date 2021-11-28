2abde60ca9b0dca2693a0e4e2a3eee42dde92d4a
Simona Aksman <scaksman@gmail.com>
2021-11-28 15:51:53 -0800

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -88,7 +88,7 @@ def calculate_capacity_progression(data, labels):
 	for size in sizes:
 		sample_data, sample_labels = get_sample(size, data, labels, 1234)
 		mec = calculate_mec(sample_data, sample_labels)
-		capacities[size] = mec * 100
+		capacities[size * 100] = mec
 
 		print("MEC for " + str(int(size * 100)) + " percent of the data: " + str(round(mec, 2)) + " bits")
 
